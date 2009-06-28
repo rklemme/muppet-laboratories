5f79c9033ac44394f9a6997a3b4dff438d9b0ba3
Robert Klemme <shortcutter@googlemail.com>
2009-06-28 16:20:33 +0200

    It turns out that Time.strptime is awfully slow, so replaced it with a more efficient parsing:
    $ time bin/sample-animal.rb sample.log
    
    real    0m54.048s
    user    0m28.202s
    sys     0m23.217s
    
    $ time bin/sample-animal.rb sample.log
    
    real    0m33.057s
    user    0m6.140s
    sys     0m23.593s

--- bin/sample-animal.rb
+++ bin/sample-animal.rb
@@ -4,13 +4,10 @@
 # generator script test-gen.rb.
 $: << File.join(File.dirname(File.dirname($0)), "lib")
 
-require 'time'
 require 'animal'
 
 # main defines the custom parser class!
 Animal.main do
-
-  TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%N'.freeze
 
   attr_accessor :year
   attr_reader :interaction_id, :time_stamp
@@ -18,13 +15,16 @@ Animal.main do
   def parse(line)
     if %r{
        ^
-       ( \d{4}-\d{2}-\d{2} \s \d{2}:\d{2}:\d{2}(?:\.\d+)? )
+       ( \d{4} ) - ( \d{2} ) - ( \d{2} ) 
+       \s
+       ( \d{2} ) : ( \d{2} ) : ( \d{2}(?:\.\d+)? )
        \s+
        (\S+) # interaction_id
        \s+
        }x =~ line
-      @time_stamp = Time.strptime $1, TIME_FORMAT
-      @interaction_id = $2
+      @time_stamp = Time.local $1.to_i, $2.to_i, $3.to_i,
+	$4.to_i, $5.to_i, $6.to_f
+      @interaction_id = $7
     else
       @time_stamp = nil
       @interaction_id = nil
