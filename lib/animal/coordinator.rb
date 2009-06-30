aefaf78da1731d68de4c8264e1d25b99eccef0db
Robert Klemme <shortcutter@googlemail.com>
2009-06-30 18:33:56 +0200

    Next step of InteractionProcessor implementation: - use strategy for state - use more complex filtering (dummy still)

--- lib/animal/coordinator.rb
+++ lib/animal/coordinator.rb
@@ -2,19 +2,21 @@
 # The coordinator is the main instance which coordinates
 # processing of log files.
 module Animal
+
   class Coordinator
 
-    YES = lambda {|processor| true}
+    YES = Class.new do
+      def first(iap, line, ts) :yes end
+      def initial(iap, line, ts) :yes end
+      def followup(iap, line) :yes end
+    end.new
 
-    attr_reader :options
+    attr_reader :options, :filter
     attr_accessor :parser
 
     def initialize
       @filter = YES
-      @processors = Hash.new do |h, id|
-	id.freeze
-	h[id] = InteractionProcessor.new(id, self)
-      end
+      @processors = Hash.new
     end
 
     def options=(opts)
@@ -68,12 +70,24 @@ module Animal
 	parser.parse line
 
 	if parser.initial_line?
-	  last_proc = @processors[parser.interaction_id]
-	  last_proc.process(parser.time_stamp, line)
+	  id = parser.interaction_id
+	  last_proc = @processors[id]
+
+	  unless last_proc
+	    # the first line of this interaction
+	    last_proc = InteractionProcessor.new(id.freeze, self)
+	    @processors[id] = last_proc
+	    last_proc.process_initial(parser.time_stamp, line)
+	  else
+	    # not the first line
+	    last_proc.process(parser.time_stamp, line)
+	  end
+
 	else
 	  last_proc.append_line line
 	end
       end
     end
   end
+
 end
--- lib/animal/interaction_processor.rb
+++ lib/animal/interaction_processor.rb
@@ -12,23 +12,112 @@ module Animal
   # processor of a single interaction from the log
   class InteractionProcessor
 
+    UNDECIDED = Class.new do
+      def process_initial(iap, line, time_stamp)
+	case iap.coord.filter.first(iap, line, time_stamp)
+	when :yes
+	  # we'll improve this once LRU is there
+	  iap.entries << Entry.new(time_stamp, line)
+	  INCLUDE
+	when :no
+	  iap.entries.clear
+	  EXCLUDE
+	when :maybe
+	  iap.entries << Entry.new(time_stamp, line)
+	  self
+	else
+	  raise 'Illegal return'
+	end
+      end
+
+      def process(iap, line, time_stamp)
+	case iap.coord.filter.initial(iap, line, time_stamp)
+	when :yes
+	  # we'll improve this once LRU is there
+	  iap.entries << Entry.new(time_stamp, line)
+	  INCLUDE
+	when :no
+	  iap.entries.clear
+	  EXCLUDE
+	when :maybe
+	  iap.entries << Entry.new(time_stamp, line)
+	  self
+	else
+	  raise 'Illegal return'
+	end
+      end
+
+      def append_line(iap, line)
+	case iap.coord.filter.followup(iap, line)
+	when :yes
+	  # we'll improve this once LRU is there
+	  l = iap.entries.last and l.line << line
+	  INCLUDE
+	when :no
+	  iap.entries.clear
+	  EXCLUDE
+	when :maybe
+	  l = iap.entries.last and l.line << line
+	  self
+	else
+	  raise 'Illegal return'
+	end
+      end
+    end.new
+
+    INCLUDE = Class.new do
+      def process_initial(iap, line, time_stamp)
+	iap.entries << Entry.new(time_stamp, line)
+	self
+      end
+
+      def process(iap, line, time_stamp)
+	iap.entries << Entry.new(time_stamp, line)
+	self
+      end
+
+      def append_line(iap, line)
+	l = iap.entries.last and l.line << line
+	self
+      end
+    end.new
+
+    EXCLUDE = Class.new do
+      def process_initial(iap, line, ts)
+	self
+      end
+
+      def process(iap, line, ts)
+	self
+      end
+
+      def append_line(iap, line)
+	self
+      end
+    end.new
+
     attr_reader :id, :coord, :entries
 
     def initialize(id, coordinator)
       @id = id
       @coord = coordinator
       @entries = []
+      @state = UNDECIDED
+    end
+
+    # Process the first line
+    def process_initial(time_stamp, line)
+      @state = @state.process_initial(self, line, time_stamp)
     end
 
     # Process an initial line
     def process(time_stamp, line)
-      # unfiltered for now
-      @entries << Entry.new(time_stamp, line)
+      @state = @state.process(self, line, time_stamp)
     end
 
     # Append a continuation line to the last entry
     def append_line(line)
-      l = @entries.last and l.line << line
+      @state = @state.append_line(self, line)
     end
 
     def finish
